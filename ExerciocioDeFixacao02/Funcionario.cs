d487eba224d98ecb0330e60844dc5c277b4d8ceb
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-18 12:11:06 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciocioDeFixacao02/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciocioDeFixacao02
+{
+    using System.Globalization;
+    class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return Salario - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario = Salario + (Salario * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
